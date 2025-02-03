d07792e014129203e6ef8330c82fb3536221e21f
JCY-3958 <w7e43958@naver.com>
2025-02-03 10:28:58 +0900

    아오 이거 어렵네

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,1 @@
 console.log("오예 성공이얌");
-
-const button = document.getElementById("open-popup");
-  
-button.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "showAlert", message: "Hello from extension!" });
-    });
-  });
-
-document.addEventListener("DOMContentLoaded", () => {
-    
-  });
--- scripts/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "showAlert") {
-    alert(request.message); // 브라우저 페이지에서 alert 창을 띄움
-    sendResponse({ success: true });
-  }
-});
